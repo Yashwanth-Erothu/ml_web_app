b45b0365b983001e642f168c4de2a9df248b4e3c
Yashwanth-Erothu <101470314+Yashwanth-Erothu@users.noreply.github.com>
2023-09-30 16:23:43 +0530

    diseases_prediction.py

--- diseases_prediction.py
+++ diseases_prediction.py
@@ -5,13 +5,13 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-# diabetes_model = pickle.load(open(r"C:\Users\Lenovo\Downloads\project\diabetes\diabetes_trained_model.sav", 'rb'))
+diabetes_model = pickle.load(open('diabetes_trained_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open(r"C:\Users\Lenovo\Downloads\project\heart disease\heart_disease_trained_model.sav", 'rb'))
+heart_disease_model = pickle.load(open('heart_disease_trained_model.sav', 'rb'))
 
-parkinsons_model = pickle.load(open(r"C:\Users\Lenovo\Downloads\project\parkinsons disease\parkinsons_trained_model.sav", 'rb'))
+parkinsons_model = pickle.load(open('parkinsons_trained_model.sav', 'rb'))
 
-anemia_model = pickle.load(open(r"C:\Users\Lenovo\Downloads\project\anemia disease\anemia_trained_model.sav", 'rb'))
+anemia_model = pickle.load(open('anemia_trained_model.sav', 'rb'))
 
 
 
